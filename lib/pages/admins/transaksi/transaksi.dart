44f81cfbd75781a592bf89ef9e178e4dd3578a04
Denny Lianto <c14190004@john.petra.ac.id>
2022-11-22 17:07:51 +0700

    Update transaksi.dart

--- lib/pages/admins/transaksi/transaksi.dart
+++ lib/pages/admins/transaksi/transaksi.dart
@@ -406,7 +406,7 @@ class _AdminTransaksiPageState extends State<AdminTransaksiPage> {
               overflow: TextOverflow.ellipsis,
             ),
           ),
-        ],
+        ], 
       ),
     );
   }
